90b68c12313170c4c65e81ac50bd10601d54ecda
Yolo1105 <ml7612@nyu.edu>
2024-04-20 11:29:23 -0400

    update

--- run.py
+++
@@ -1,7 +0,0 @@
-from nyct_gtfs import NYCTFeed
-
-feed = NYCTFeed("1", api_key="https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace")
-
-trains = feed.trips
-
-print(len(trains))
--- service_alert.js
+++ service_alert.js
@@ -2,37 +2,43 @@ document.addEventListener('DOMContentLoaded', function() {
     const statusDisplay = document.getElementById('service-alerts-status');
 
     async function fetchServiceAlerts() {
-        const url = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fall-alerts.json';
+        const url = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts.json';
         try {
             const response = await fetch(url);
-            if (!response.ok) throw new Error('Network response was not ok');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
             const data = await response.json();
+            console.log('Alerts data:', data); 
             displayServiceAlerts(data);
+            
         } catch (error) {
             statusDisplay.innerHTML = `Failed to load data: ${error.message}`;
         }
-    }
+    }    
     
     function displayServiceAlerts(alertsData) {
         statusDisplay.innerHTML = ''; 
-
-        alertsData.forEach((alert) => {
-            const div = document.createElement('div');
-            div.className = 'service-alert-entry';
-            div.innerHTML = `
-                <h2 data-translate="alertTitle">${alert.title}</h2>
-                <p><strong data-translate="description">Description:</strong> ${alert.description}</p>
-                <p><strong data-translate="updatedAt">Updated At:</strong> ${alert.updatedAt}</p>
-                // Add more elements as needed based on the alert data structure
-            `;
-            statusDisplay.appendChild(div);
-        });
-
-        translatePage(document.getElementById('language-selector').value);
-    }
     
-    function translatePage(language) {
-        // translation
+        if (Array.isArray(alertsData) && alertsData.length > 0) {
+            alertsData.forEach((alert) => {
+                const div = document.createElement('div');
+                div.className = 'service-alert-entry';
+                div.style.backgroundColor = '#f4f4f4'; 
+                div.style.border = '1px solid #ddd';
+                div.style.borderRadius = '5px';
+                div.style.padding = '10px'; 
+    
+                div.innerHTML = `
+                    <h2>${alert.title}</h2>
+                    <p>${alert.description}</p>
+                    <p>Updated At: ${alert.updatedAt}</p>
+                `;
+                statusDisplay.appendChild(div);
+            });
+        } else {
+            statusDisplay.innerHTML = 'No active alerts.';
+        }
     }
 
     fetchServiceAlerts();
